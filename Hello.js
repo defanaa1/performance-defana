39b75989b14dfcadcfb4c795143bd27f337680fe
Defana Aditya <defanaa1@gmail.com>
2023-08-02 16:21:29 +0700

    commit pertamakali

---
+++ Hello.js
@@ -0,0 +1,11 @@
+const name = "Ashutosh";
+
+const displayMessage = () => {
+    return "I need Help!";
+};
+
+function Hello(){
+    return <h1>The message is: {displayMessage()}!</h1>
+}
+
+export default Hello;
